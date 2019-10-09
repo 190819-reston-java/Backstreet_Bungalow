ed9d7cde9d55e37120ab912858359ba3da6d7247
Carlos Santos <carlosasantos17@gmail.com>
2019-10-09 13:20:38 -0400

    Added Messages and Posts beans

---
+++ projectTwo/src/main/java/com/revature/beans/Posts.java
@@ -0,0 +1,65 @@
+package com.revature.beans;
+
+public class Posts {
+	
+	private long Id;
+	private String userId;
+	private String content;
+	private String title;
+	
+	public Posts() {
+		Id = 0;
+		userId = content = title = "";
+	}
+
+	public Posts(long id, String userId, String content, 
+			String title) {
+		Id = id;
+		this.userId = userId;
+		this.content = content;
+		this.title = title;
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "Posts [Id=" + Id + ", userId=" + userId 
+				+ ", content=" + content + ", title=" + title + "]";
+	}
+	
+	
+	
+	
+	
+}
