d6a3c8e22d8f7653bb0e2aa7cc782d3db2d421fd
Nick Brinson <nickbrinson3@gmail.com>
2019-10-11 16:51:19 -0400

    login component changes

--- projectTwo/src/main/webapp/angular-bsb-watch/src/app/login/login.component.ts
+++ projectTwo/src/main/webapp/angular-bsb-watch/src/app/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  loginImage:any = "assets\\images\\bb4.jpg";
+  title = 'app';
 
   constructor() { }
 
