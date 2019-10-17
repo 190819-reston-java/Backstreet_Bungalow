079736b6d92cfa879d73e7c49f8bdebe54c3656b
Carlos Santos <carlosasantos17@gmail.com>
2019-10-17 14:02:24 -0400

    Got rid of numOfComments in Posts class.

--- projectTwo/src/main/java/com/revature/beans/Posts.java
+++ projectTwo/src/main/java/com/revature/beans/Posts.java
@@ -35,14 +35,13 @@ public class Posts {
 		super();
 	}
 
-	public Posts(long id, long postId, String userId, String title, String content, long numOfComments) {
+	public Posts(long id, long postId, String userId, String title, String content) {
 		super();
 		this.id = id;
 		this.postId = postId;
 		this.userId = userId;
 		this.title = title;
 		this.content = content;
-		this.numOfComments = numOfComments;
 	}
 
 	public long getId() {
@@ -122,8 +121,6 @@ public class Posts {
 			return false;
 		if (id != other.id)
 			return false;
-		if (numOfComments != other.numOfComments)
-			return false;
 		if (postId != other.postId)
 			return false;
 		if (title == null) {
@@ -142,7 +139,7 @@ public class Posts {
 	@Override
 	public String toString() {
 		return "Posts [id=" + id + ", postId=" + postId + ", userId=" + userId + ", title=" + title + ", content="
-				+ content + ", numOfComments=" + numOfComments + "]";
+				+ content + "]";
 	}
 
 	
