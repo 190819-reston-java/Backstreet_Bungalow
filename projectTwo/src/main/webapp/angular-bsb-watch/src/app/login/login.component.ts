962e17b70fd7ab3d9305099e6e2963eabc2a3118
Nick Brinson <nickbrinson3@gmail.com>
2019-10-19 19:32:23 -0400

    login functionality implemented on front-end

--- projectTwo/src/main/webapp/angular-bsb-watch/src/app/login/login.component.ts
+++ projectTwo/src/main/webapp/angular-bsb-watch/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { CurrentUserService } from '../current-user.service';
 import { User } from '../user';
+import { Router } from '@angular/router'
 
 
 
@@ -17,17 +18,34 @@ export class LoginComponent implements OnInit {
 
   }
 
+
   private loginURL = 'http://localhost:8080/Project2/login';
 
 
-  constructor(private http: HttpClient, private currentUser: CurrentUserService) { }
+  constructor(
+    private http: HttpClient,
+    private currentUser: CurrentUserService,
+    private router: Router) { }
+
   user: User = new User();
   onSubmit() {
-    console.log(this.user);
-    let observable = this.http.post(this.loginURL, JSON.stringify(this.user))
-    observable.subscribe((result: any) => {
-      this.currentUser.user = result;
-    })
+
+    let observable = this.http.post(this.loginURL, JSON.stringify(this.user));
+
+    observable.subscribe(
+      (result: any) => {
+        this.currentUser.user = result;
+        console.log(this.currentUser.user);
+        if (this.currentUser.user) {
+          this.router.navigate(['profile']);
+        }
+      },
+      (err) => { alert("Incorrect username or password. Please try again.") }
+    );
+
+
+
+
   }
 
 }
