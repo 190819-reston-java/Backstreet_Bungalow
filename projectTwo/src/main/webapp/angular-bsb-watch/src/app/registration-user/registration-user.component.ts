c35d1e0309142f14b6ccf6af98e40fc663ed9be8
Nick Brinson <nickbrinson3@gmail.com>
2019-10-19 22:04:45 -0400

    registration section completely implemented

--- projectTwo/src/main/webapp/angular-bsb-watch/src/app/registration-user/registration-user.component.ts
+++ projectTwo/src/main/webapp/angular-bsb-watch/src/app/registration-user/registration-user.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from "../user";
 import { HttpClient } from '@angular/common/http';
 import { CurrentUserService } from '../current-user.service';
- 
+import { Router } from '@angular/router';
+
 
 
 
@@ -13,27 +14,37 @@ import { CurrentUserService } from '../current-user.service';
 })
 export class RegistrationUserComponent implements OnInit {
 
-  constructor(private http: HttpClient, private currentUser: CurrentUserService) {
-
+  constructor(
+    private http: HttpClient, 
+    private currentUser: CurrentUserService,
+    private router: Router) {
   }
 
   ngOnInit() {
 
   }
 
+  favoriteBoy: any;
+
   user: User = new User();
 
   registerUrl: string = "http://localhost:8080/Project2/addNewUser";
 
 
-  onSubmit()  {
+  onSubmit() {
     console.log(this.user);
 
     let observable = this.http.post(this.registerUrl, JSON.stringify(this.user));
 
     observable.subscribe(
-      (result: any) => {this.currentUser.user = result;},
-      (err) => {alert("Information already recognized in database, please try again or login");}  
+      (result: any) => {
+        this.currentUser.user = result;
+        if (this.currentUser.user) {
+          alert("Successfully registered!");
+          this.router.navigate(['profile']);
+        }
+      },
+      (err) => { alert("Information already recognized in database, please try again or login"); }
     )
   }
 }
